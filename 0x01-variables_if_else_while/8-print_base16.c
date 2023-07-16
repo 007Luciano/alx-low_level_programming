9e3e714a9a9a668a2f9efb48364c2e1a692ab6fb
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-16 16:44:25 -0700

    hex

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,12 @@ int main(void)
 {
 char hex;
 
-for (hex = '0'; hex <= '9'; hex++) {
+for (hex = '0'; hex <= '9'; hex++)
+{
 putchar(hex);
 }
-for (hex = 'a'; hex <= 'f'; hex++) {
+for (hex = 'a'; hex <= 'f'; hex++)
+{
 putchar(hex);
 }
 putchar('\n');
