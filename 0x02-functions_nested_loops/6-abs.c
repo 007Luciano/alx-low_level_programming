9f3c3fd88e6cda2ec237fb847ad66f5069032d43
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-18 20:12:26 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: integer check
+ * Return: n negation for integer value
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
