b065f36e003509e863456764c1ccd1691fd98b58
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-19 15:12:37 -0700

    Prints last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
  * print_last_digit - Entry point
- * @n: int of the last digit
+ * @n: last digit extracted from arguement
+ * @i: arguement passed for extraction of last digit
  * Return: value of last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int i)
 {
-int i;
-
-if (i)
+int n = i % 10;
+char nc = '0' + n;
+_putchar(nc);
+_putchar('\n');
+return (n);
 }
