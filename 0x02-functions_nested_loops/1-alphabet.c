6111121a854dfaf21c834704251fb811528f2eb0
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-18 18:52:20 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,11 @@
 #include "main.h"
-
 /**
- * main - Entry point
- * print_alphabet - function printing the alphabet
+ * print_alphabet - Entry point
+ * 
  * Return: Always 0.
  */
-
 void print_alphabet(void)
-{
-char c = 'a';
-
+{char c = 'a';
 while (c <= 'z')
 {
 _putchar(c);
