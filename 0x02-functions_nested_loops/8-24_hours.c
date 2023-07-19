c6a9b70d79b8b3935e53a0691736350b26728e7f
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-19 08:58:58 -0700

    federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * 
+ *
  * starting from 00:00 to 23:59
  *
  */
@@ -10,11 +10,9 @@ void jack_bauer(void)
 int h, m;
 
 h = 0;
-
 while (h < 24)
 {
 m = 0;
-
 while (m < 60)
 {
 _putchar((h / 10) + '0');
