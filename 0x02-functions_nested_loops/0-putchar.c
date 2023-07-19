b265749d7509e2e843c9ca854f63bde779e672f0
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-18 18:07:42 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,15 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succes)
  */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
 int main(void)
 {
-_putchar('\n');
 putchar('\n');
 return (0);
 }
