e95ed9943f31cd8248e3cc9428fb104e9efc961b
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-21 11:55:48 -0700

    I believe in numbers and signs

---
+++ 0x03-debugging/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ * Return: returns nothing
+ */
+void print_most_numbers(void)
+{
+char c;
+
+for (c = '0'; c < 10; c++)
+{
+if (!(c == '2' || c == '4'))
+	_putchar(c);
+}
+_putchar('\n');
+}
