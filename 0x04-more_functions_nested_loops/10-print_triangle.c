8916f004c82e7b8225eb2c586ce10194c5de9523
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-21 19:57:22 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,13 +13,13 @@ else
 {
 int i, j;
 
-for (i = 1; i < size; i++)
+for (i = 1; i <= size; i++)
 {
 for (j = i; j < size; j++)
 {
 _putchar(' ');
 }
-for (j = 1; j < i; j++)
+for (j = 1; j <= i; j++)
 {
 _putchar('#');
 }
