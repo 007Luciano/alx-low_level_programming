5419eee0ddf7b489f4651569cfb3d5ce07fdcb48
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-24 13:15:39 -0700

    I can only go one way

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: sting character to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int l = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	s--;
+	for (i = l; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
