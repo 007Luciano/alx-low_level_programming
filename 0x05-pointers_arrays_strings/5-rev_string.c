0397585d23da465ffb3a61577931c998b9072477
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-24 19:32:10 -0700

    A good engineer thinks in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,28 +3,29 @@
 /**
  * rev_string - reverses a string
  * @s: string character to be reversed
+ * Return: String in reverse
  */
 
 void rev_string(char *s)
 {
-        int l = 0;
+	int l = 0;
 	char *last = s;
 
-        while (*last != '\0')
-        {
-                l++;
-                s++;
-        }
-        last--;
+	while (*last != '\0')
+	{
+		l++;
+		s++
+	}
+	last--;
 
 	char hold;
 
-        while (s < last)
-        {
-                hold = *s;
+	while (s < last)
+	{
+		hold = *s;
 		*s = last;
 		*last = hold;
-                s++;
+		s++;
 		last--;
-        }
+	}
 }
