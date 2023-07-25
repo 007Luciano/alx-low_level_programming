fb2398c9cb06ef4c913c63c84368cf0439e36da9
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-25 06:37:07 -0700

    copy this string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,25 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws diagonal lines
- * @n: number of times \ should print
+ * *_strcpy - copies the string pointed to by src
+ * @dest: copy to this
+ * @src: copy from
+ * Return: string
  */
 
 char *_strcpy(char *dest, char *src)
 {
+	int i = 0;
+	int j = 0;
 
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	for ( ; j < i; j++)
+	{
+		dest[j] = src[x];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
