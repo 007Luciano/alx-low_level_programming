d5436e665baa5aba2ec40fd616bc983addbd1b31
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-07-27 10:53:43 -0700

    Mozart composed his music not for the elite

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,14 +3,14 @@
 /**
  * *string_toupper - changes all lowercase letters of a string to uppercase
  * @s: pointer
- * Return: s
+ * Return: a pointer to string
  */
 
 char *string_toupper(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
