c8977d2d3a7d74f5c0b3a4c58cbb49c28d2a679c
Lucky Fatch <lucky.h.fatch@gmail.com>
2023-08-16 06:25:39 -0700

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,14 +1,12 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - executes a function given as a param on each array element
  * @array: array
  * @size: how many elem to print
  * @action: pointer to print in regular or hex
  * Return: void
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
